54b0d6e4184ffa5f7b8dde412d8bff5ad09ee853
Evgeny Ermolaev <ermolaev.frontend@gmail.com>
2025-07-13 22:10:49 +0400

    Refactor Interaction class for improved clarity and structure
    * Simplified constructor by directly defining properties as readonly fields, enhancing readability.
    * Removed the reset method to streamline the class functionality, focusing on essential interactions.

--- src/entities/canvas/classes/Interaction.ts
+++ src/entities/canvas/classes/Interaction.ts
@@ -6,48 +6,17 @@ export type Handle = 'nw' | 'n' | 'e' | 'ne' | 'se' | 's' | 'sw' | 'w' | 'rotate
 const systemCenter: Point = { x: 0, y: 0 };
 
 export default class Interaction {
-   
-  constructor(
-    readonly shape: IShape | null = null,
-    readonly type: InteractionType = 'idle',
-    readonly dragOffset: Point = systemCenter,
-    readonly handle: Handle | null = null,
-    readonly startPoint: Point = systemCenter,
-    readonly initialAngle: number = 0,
-    readonly startRotation: number = 0,
-    readonly initialPoints: Point[] = [],
-    readonly initialBounds: Bounds = { x: 0, y: 0, width: 0, height: 0 },
-    readonly panOffset: Point = systemCenter,
-  ) {
-    // Parameter properties automatically create and assign the fields
-    this.shape = shape;
-    this.type = type;
-    this.dragOffset = dragOffset;
-    this.handle = handle;
-    this.startPoint = startPoint;
-    this.initialAngle = initialAngle;
-    this.startRotation = startRotation;
-    this.initialPoints = initialPoints;
-    this.initialBounds = initialBounds;
-    this.panOffset = panOffset;
-  }
+  readonly type: InteractionType = 'idle';
+  readonly shape: IShape | null = null;
+  readonly dragOffset: Point = systemCenter;
+  readonly handle: Handle | null = null;
+  readonly initialAngle: number = 0; // angle between Ox and shape handle in rad when rotation is started
+  readonly startRotation: number = 0; // shape.rotation when rotation is started in rad
+  readonly initialPoints: Point[] = [];
+  readonly initialBounds: Bounds = { x: 0, y: 0, width: 0, height: 0 };
+  readonly panOffset: Point = systemCenter;
 
   patch(inter: Partial<Interaction>) {
     Object.assign(this, inter);
-  }
-
-  reset() {
-    this.patch({
-      type: 'idle',
-      shape: null,
-      dragOffset: systemCenter,
-      handle: null,
-      startPoint: systemCenter,
-      initialAngle: 0,
-      startRotation: 0,
-      initialPoints: [],
-      initialBounds: { x: 0, y: 0, width: 0, height: 0 },
-      panOffset: systemCenter,
-    });
   }
 }
